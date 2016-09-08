e9fcaef1e84ed56764131a9951ba60bb1a85e3dd
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-08 18:17:25 +0300

    lis

--- lab2_4.py
+++ lab2_4.py
@@ -13,15 +13,13 @@ a=0
 pylab.ion()
 
 for t in range (50):
-          xlist = [math.sin (t + a) for x in glist]
-          ylist = [math.cos(2*t) for y in glist]
-pylab.clf()
-pylab.plot (xlist, ylist)
-
-pylab.draw()
-a +=0.5
+    xlist = [math.sin (t + a) for t in glist]
+    ylist = [math.cos(2*t) for t in glist]
+    pylab.clf()
+    pylab.plot (xlist, ylist)
+    pylab.draw()
+    a +=0.5
 pylab.pause(0.3)
-
 
 
 pylab.close()
