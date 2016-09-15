8970f8b3a91861b8ff51a728886efe41989b297d
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-15 18:07:35 +0300

    normal'no

---
+++ lab3/3.10.py
@@ -0,0 +1,9 @@
+__author__ = 'student'
+import turtle
+turtle.shape('turtle')
+turtle.speed(10)
+for i in range(6):
+    for j in range(361):
+        turtle.forward(1)
+        turtle.left(1)
+    turtle.right(60)
